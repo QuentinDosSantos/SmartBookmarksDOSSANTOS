c9a5ec8f59dff90161d293b66c01ad28ce9a5678
QuentinDOSSANTOS <quentin.dossantos@yandex.com>
2018-04-05 14:52:26 +0200

    Initial commit

---
+++ app/src/main/java/smartbookmarks/dossantos/diiage/org/smartbookmarksdossantos/MainActivity.java
@@ -0,0 +1,13 @@
+package smartbookmarks.dossantos.diiage.org.smartbookmarksdossantos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
